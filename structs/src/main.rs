9c5b8f9f28e82517977f5ca5a21467d2c23a7bb3
lanzy <lanzy@wanyantech.com>
2024-01-01 14:18:54 +0800

    🎉 complete structs

---
+++ structs/src/main.rs
@@ -0,0 +1,78 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+struct AlwaysEqual;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect0 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+
+    println!("Can rect1 hold rect0? {}", rect1.can_hold(&rect0));
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+    let squ = Rectangle::square(3);
+    dbg!(squ);
+
+    let mut user1 = build_user(
+        String::from("someusername123"),
+        String::from("someone@example.com"),
+    );
+
+    user1.email = String::from("anotheremail@example.com");
+    println!("\nuser: {:#?}", user1);
+    let _subject = AlwaysEqual;
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
