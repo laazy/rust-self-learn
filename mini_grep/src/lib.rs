0fa0c85e962773d979257aa25ce58045574ed7bf
laazy <laaazy@sjtu.edu.cn>
2024-02-12 05:31:49 +0800

    🔥 make code clear

--- mini_grep/src/lib.rs
+++ mini_grep/src/lib.rs
@@ -50,15 +50,8 @@ impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next();
 
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
-
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file path"),
-        };
+        let query = args.next().ok_or("Didn't get a query string")?;
+        let file_path = args.next().ok_or("Didn't get a file path")?;
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
