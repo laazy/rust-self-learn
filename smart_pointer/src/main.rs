ef0a4ff708ec70ea60f9f2ef615ae6f104e2334f
laazy <laaazy@sjtu.edu.cn>
2024-02-14 23:21:45 +0800

    🎨format

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -109,47 +109,47 @@ fn main() {
 }
 
 fn rc() {
-        // Rc<T> for multiple ownership
-        enum List {
+    // Rc<T> for multiple ownership
+    enum List {
         Cons(i32, Rc<List>),
         Nil,
-        }
-    
+    }
+
     use List::{Cons, Nil};
-    
+
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-        println!("count after creating a = {}", Rc::strong_count(&a));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let _b = Cons(3, Rc::clone(&a));
-        println!("count after creating b = {}", Rc::strong_count(&a));
-        {
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
         let _c = Cons(4, Rc::clone(&a));
-            println!("count after creating c = {}", Rc::strong_count(&a));
-        }
-        println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn ref_cell() {
-        // RefCell<T> and the Interior Mutability Pattern
-        #[derive(Debug)]
-        enum List {
+    // RefCell<T> and the Interior Mutability Pattern
+    #[derive(Debug)]
+    enum List {
         Cons(Rc<RefCell<i32>>, Rc<List>),
         Nil,
-        }
-    
+    }
+
     use List::{Cons, Nil};
-    
-        let value = Rc::new(RefCell::new(5));
-    
+
+    let value = Rc::new(RefCell::new(5));
+
     let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    
+
     let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
     let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
-    
-        *value.borrow_mut() += 10;
-    
-        println!("a after = {:?}", a);
-        println!("b after = {:?}", b);
-        println!("c after = {:?}", c);
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
 
 fn reference_loop() {
@@ -168,4 +168,26 @@ fn reference_loop() {
             }
         }
     }
+
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
